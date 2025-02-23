16d2e3c4b006d7df201dd6711a14433f4ebd5dba
jonathan03ant <jonathan03ant@gmail.com>
2025-02-23 14:42:57 -0500

    RAM sim init

---
+++ sim_ram.h
@@ -0,0 +1,18 @@
+#ifndef SIM_RAM_H
+#define SIM_RAM_H
+
+#define RAM_SIZE 100            //We will have 99 addressable cells, (100 inc null pointer)
+
+typedef struct {
+    int memory_cell [RAM_SIZE];
+}RAM;
+
+
+void init_RAM (RAM *ram);
+int read_memory_cell(const RAM *ram, int index);
+int write_to_memory_cell(RAM *ram, int value);
+void print_memory_cell(RAM* ram, int cell);
+void print_RAM(RAM *ram);
+int load_program(RAM *ram, const int program[], int size);
+
+#endif SIM_RAM_H
