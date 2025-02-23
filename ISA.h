73d1229c576c91189c910c666b7e29878e6dfdb4
jonathan03ant <jonathan03ant@gmail.com>
2025-02-23 14:23:45 -0500

    ISA sets, structured

--- ISA.h
+++ ISA.h
@@ -8,28 +8,31 @@
 
 typedef enum {
     /* I/O Instructions */
-    INP  = 90,   // Input number into ACC                      Class: I/O
-    OUT  = 07,   // Output ACC value                           Class: I/O    
+    INP  = 1,     // Input number into ACC                        Class: I/O
+    OUT  = 99,    // Output ACC value                             Class: I/O    
 
     /* Memory Instructions */
-    LDD = 01,   // Load value from memory into ACC            Class: Mem
-    STR = 02,  // Store ACC value into memory                Class: Mem
+    LDD = 10,     // Load value from memory into ACC              Class: Mem
+    STR = 20,     // Store ACC value into memory                  Class: Mem
+    PSH = 30,     // Push Value on to the stack                   Class: Mem
+    POP = 40,     // Pop Value from the stack                     Class: Mem
 
     /* Arithmetic Instructions */
-    ADD  = 03,   // Add memory value into ACC                  Class: Arth
-    SUB  = 04,   // Subtract memory value from ACC             Class: Arth
-    MUL  = 12,   // Multiply ACC by memory value               Class: Arth
-    DIV  = 13,   // Divide ACC by memory value                 Class: Arth
-    CMP  = 09,   // Compare ACC with memory                    Class: Arth
+    ADD  = 12,    // Add memory value into ACC                    Class: Arth
+    SUB  = 22,    // Subtract memory value from ACC               Class: Arth
+    MUL  = 32,    // Multiply ACC by memory value                 Class: Arth
+    DIV  = 42,    // Divide ACC by memory value                   Class: Arth
+    CMP  = 52,    // Compare ACC with memory                      Class: Arth
 
     /* Control Flow Instructions */
-    JMP  = 05,   // Unconditional Jump                         Class: Ctl
-    BRZ  = 06,   // Jump only if ACC is zero                   Class: Ctl
-    BRP  = 10,   // Branch if ACC > 0                          Class: Ctl
-    BRN  = 11,   // Branch if ACC < 0                          Class: Ctl
+    JMP  = 5,     // Unconditional Jump                           Class: Ctl
+    BRZ  = 15,    // Jump only if ACC is zero                     Class: Ctl
+    BRP  = 25,    // Branch if ACC > 0                            Class: Ctl
+    BRN  = 35,    // Branch if ACC < 0                            Class: Ctl
 
     /* Execution Control */
-    HLT  = 08    // Halt execution                             Class: Exe
+    HLT  = 79,    // Halt execution                               Class: Exe
+    ERR  = 89,    // Error                                        Class: Exe
 } ISA;
 
 #endif // ISA_H
