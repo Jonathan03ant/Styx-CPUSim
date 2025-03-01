df1645391bb561ea06050b930d3551590d9e4002
jonathan03ant <jonathan03ant@gmail.com>
2025-03-01 16:18:04 -0500

    Name Change on Registers, get special register implemented

--- registers.c
+++ registers.c
@@ -1,4 +1,5 @@
 #include "registers.h"
+#include "string.h""
 
 /*
     * This function initializes the registers our cpu require to operate
@@ -10,20 +11,20 @@ void init_registers (Registers *regs)
     /*
         initialize Special Registers
     */
-    regs->sp_rgstr.PC  = 0;
-    regs->sp_rgstr.ACC = 0;
-    regs->sp_rgstr.MAR = 0;
-    regs->sp_rgstr.MDR = 0;
-    regs->sp_rgstr.CIR = 0;
-    regs->sp_rgstr.UTG = 0;
+    regs->_cpu_sp_rgstr.PC  = 0;
+    regs->_cpu_sp_rgstr.ACC = 0;
+    regs->_cpu_sp_rgstr.MAR = 0;
+    regs->_cpu_sp_rgstr.MDR = 0;
+    regs->_cpu_sp_rgstr.CIR = 0;
+    regs->_cpu_sp_rgstr.UTG = 0;
 
     /*
         Inittizlize General Purpose Registers
     */
     for (int i = 0; i < 10; i++)
     {
-        regs->gn_rgstr.R->value = 0;
-        regs->gn_rgstr.R->in_use = 0;
+        regs->_cpu_gn_rgstr.R->value = 0;
+        regs->_cpu_gn_rgstr.R->in_use = 0;
     }
 }
 
@@ -41,12 +42,12 @@ void print_registers(const Registers *regs) {
     printf("| %-4s | %-4s | %-4s | %-4s | %-4s | %-4s |\n",
            "PC", "ACC", "MAR", "MDR", "CIR", "UTG");
     printf("| %4d | %4d | %4d | %4d | %4d | %4d |\n",
-           regs->sp_rgstr.PC,
-           regs->sp_rgstr.ACC,
-           regs->sp_rgstr.MAR,
-           regs->sp_rgstr.MDR,
-           regs->sp_rgstr.CIR,
-           regs->sp_rgstr.UTG);
+           regs->_cpu_sp_rgstr.PC,
+           regs->_cpu_sp_rgstr.ACC,
+           regs->_cpu_sp_rgstr.MAR,
+           regs->_cpu_sp_rgstr.MDR,
+           regs->_cpu_sp_rgstr.CIR,
+           regs->_cpu_sp_rgstr.UTG);
     printf("-------------------------------------------\n");
 
     /* ─────── General-Purpose Registers ─────── */
@@ -57,10 +58,33 @@ void print_registers(const Registers *regs) {
     for (int i = 0; i < 10; i++) {
         printf("| R%-3d | %5d |   %-3s  |\n",
                i,
-               regs->gn_rgstr.R[i].value,
-               regs->gn_rgstr.R[i].in_use ? "YES" : "NO");
+               regs->_cpu_gn_rgstr.R[i].value,
+               regs->_cpu_gn_rgstr.R[i].in_use ? "YES" : "NO");
     }
     printf("-------------------------------------------\n");
 
     printf("\n[End of Register Dump]\n\n");
+}
+
+int  get_cpu_special_registers(const Registers *regs, const char *reg_name)
+{
+    if (strcmp(reg_name, "PC")  == 0)
+        return regs->_cpu_sp_rgstr.PC;
+    if (strcmp(reg_name, "ACC") == 0)
+        return regs->_cpu_sp_rgstr.ACC;
+    if (strcmp(reg_name, "MAR") == 0)
+        return regs->_cpu_sp_rgstr.MAR;
+    if (strcmp(reg_name, "MDR") == 0)
+        return regs->_cpu_sp_rgstr.MDR;
+    if (strcmp(reg_name, "UTG") == 0)
+        return regs->_cpu_sp_rgstr.UTG;
+
+    // Error handling for unknown register name
+    printf("__ERR_RGSTR: unknown register name '%s'\n, reg_name");
+    return -1;
+}
+
+int get_cpu_general_registers()
+{
+
 }
--- registers.h
+++ registers.h
@@ -8,33 +8,33 @@ typedef struct {
     int MDR;                                     //Memory Data Register: Holds data read from or written to memory 
     int CIR;                                     //Current Instruction Register: Holds fetched instruction
     int UTG;                                     //My Own Register :D
-} Special_Register;
+} __cpu_Special_Register;
 
 typedef struct {
     int value;
     int in_use;
-} Gen_rgstr;
+} __cpu_Gen_rgstr;
 
 typedef struct {
-    Gen_rgstr R[10];                                   //Array of 10 general registers(R0-R9)
-} General_Register;
+    __cpu_Gen_rgstr R[10];                                   //Array of 10 general registers(R0-R9)
+} __cpu_General_Register;
 
 typedef struct {
-    Special_Register sp_rgstr;                          //Special Purpose Registers
-    General_Register gn_rgstr;                           //General Registers
+    __cpu_Special_Register _cpu_sp_rgstr;                          //Special Purpose Registers
+    __cpu_General_Register _cpu_gn_rgstr;                           //General Registers
 
 } Registers;
 
 
 
 void init_registers(Registers *regs);
-void print_regisrers(const Registers *regs);
+void print_registers(const Registers *regs);
 
-int  get_special_registers(const Registers *regs, const char *reg_name);
-int  get_general_register(const Registers *regs, int reg_index); 
+int  get_cpu_special_registers(const Registers *regs, const char *reg_name);
+int  get_cpu_general_register(const Registers *regs, int reg_index); 
 
-int  set_special_register( Registers *regs, const char *reg_name, int value);
-int  set_general_register(Registers *regs, int reg_index, int value);
+int  set_cpu_special_register( Registers *regs, const char *reg_name, int value);
+int  set_cpu_general_register(Registers *regs, int reg_index, int value);
 
 
 
