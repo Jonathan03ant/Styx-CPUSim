832c0b6d0a9169acfe0d4868b7eb82dccce50fb1
jonathan03ant <jonathan03ant@gmail.com>
2025-06-14 00:03:42 -0400

    [Operand.c] Decoding functions defined Implementation pendign

--- src/isa/operand.c
+++ src/isa/operand.c
@@ -1,2 +1,20 @@
-#include <isa.h>
-#include <operand.h>
+#include "isa.h"
+#include "operand.h"
+
+
+void decode_operand_r(uint16_t raw, void *Out){
+    OperandR *r = (OperandR *)Out;
+    //split raw bit, assign to registers
+}
+
+
+void decode_operand_i(uint16_t raw, void *Out){
+    OperandI *i = (OperandI *)Out;
+    //split the raw operand bit
+    //assign to registers
+}
+
+void decode_operand_j(uint16_t raw, void *Out){
+    OperandJ *j = (OperandJ *)Out;
+    
+}
