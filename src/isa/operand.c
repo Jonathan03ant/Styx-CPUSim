7dfee05775dce74bf3ed03006be58484b86efe5f
jonathan03ant <jonathan03ant@gmail.com>
2025-06-15 22:39:34 -0400

    [operand.c] Implemented decoding logic fr all three type registers * Shifting then masking(extracting) (raw > n) & MASK J type takes all 12 bits (No masking)

--- src/isa/operand.c
+++ src/isa/operand.c
@@ -4,17 +4,50 @@
 
 void decode_operand_r(uint16_t raw, void *Out){
     OperandR *r = (OperandR *)Out;
-    //split raw bit, assign to registers
+    /*
+        [opcode 4 bits] [raw 12 bits]
+        raw = 0000 0000 0000
+        
+        Rd = bit 8-11
+        Rs1 = bit 4-7
+        Rs2 = bit 0-3
+
+        raw = 0xAB3 = 10:11:3 and 1010 1011 0011
+        1011 1011 0011
+        rd = shift, extract
+        rd = (raw >> 8 ) & 0xF
+        raw >> 8 = 1010 1011 0011 >> 8 = 0000 0000 1010
+        then 0000 0000 1010 & 0xF (Extract)
+        0000 0000 1010
+                  1111
+             rd = 1011
+
+        rs1 = shift, extract (middle)
+        rd = (raw >> 4) & 0xF
+        raw >> 4 = 1010 1011 0011 >> 4 =  0000 1010 1011
+        then 0000 1010 1011  & 0xF
+        0000 1010 1011
+             0000 1111
+            rs1 = 1011
+    */
+    r->rd = (raw >> 8) & 0xF;
+    r->rs1 = (raw >> 4) & 0xF;
+    r->rs2 = (raw) & 0xF;
+    
 }
 
 
 void decode_operand_i(uint16_t raw, void *Out){
     OperandI *i = (OperandI *)Out;
-    //split the raw operand bit
-    //assign to registers
+
+    i->rd = (raw >> 8) & 0xF;
+    i->rs1 = (raw >> 4) & 0xF;
+    i->imm = (raw) & 0xF;
+
 }
 
 void decode_operand_j(uint16_t raw, void *Out){
     OperandJ *j = (OperandJ *)Out;
-    
+    j->imm = raw;
+
 }
