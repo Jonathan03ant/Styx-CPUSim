8c9c8a3167f31b3e91f666cb6bbbc72eb3c03748
jonathan03ant <jonathan03ant@gmail.com>
2025-06-20 23:51:45 -0400

    [isa.c] ISA_LUT definition, indexes defined according to enum

--- src/isa/isa.c
+++ src/isa/isa.c
@@ -1,3 +1,55 @@
 #include "isa.h"
 #include "isa_if.h"
 #include "operand.h"
+
+extern const ISA_defn ISA_LUT[ISA_OP_COUNT] = {
+    [ISA_OP_NOP_I]      = {
+        
+    },
+    [ISA_OP_MOV_R]      = {
+
+    },
+    [ISA_OP_LOAD_I]     = {
+
+    },
+    [ISA_OP_STORE_I]    = {
+
+    },
+    [ISA_OP_ADD_R]      = {
+
+    },
+    [ISA_OP_SUB_R]      = {
+
+    },
+    [ISA_OP_MUL_R]      = {
+
+    },
+    [ISA_OP_DIV_R]      = {
+
+    },
+    [ISA_OP_ADDI_I]     = {
+
+    },
+    [ISA_OP_CMP_R]      = {
+
+    },
+    [ISA_OP_JMP_J]      = {
+
+    },
+    [ISA_OP_JZ_J]       = {
+
+    },
+    [ISA_OP_JNZ_J]      = {
+
+    },
+    [ISA_OP_CALL_J]     = {
+
+    },
+    [ISA_OP_RET_J]      = {
+
+    },
+    [ISA_OP_IO_I]       = {
+
+    },
+
+};
