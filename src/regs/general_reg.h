c0f22fdf1d0100a134307dfb7c3b720557890b81
jonathan03ant <jonathan03ant@gmail.com>
2025-07-12 21:42:25 -0400

    [general_regs.h] ifdef for c++, added a context struct for General Regs, added additional apis

--- src/regs/general_reg.h
+++ src/regs/general_reg.h
@@ -1,6 +1,11 @@
 #ifndef GENERAL_REG_H
 #define GENERAL_REG_H
 #include "stdint.h"
+#include "stdbool.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef enum 
 {
@@ -16,7 +21,6 @@ typedef enum
     REG_Gen_R9,
     REG_Gen_R10,
     REG_Gen_R11,
-    REG_Gen_R11,
     REG_Gen_R12,
     REG_Gen_R13,
     REG_Gen_R14,
@@ -25,16 +29,26 @@ typedef enum
 }__cpu_General_Register;
 
 
+typedef struct {
+    uint16_t regs[REG_COUNT_GEN];
+}GenRegister_ctx; //Register context snapshot
+
+
 void GEN_InitRegs(void);
-uint16_t GEN_GetRegs(__cpu_General_Register regs);
-void GEN_SetRegs(__cpu_General_Register regs, uint16_t value);
+bool Is_Valid(__cpu_General_Register reg);
+uint16_t GEN_GetReg(__cpu_General_Register reg);
+void GEN_SetReg(__cpu_General_Register reg, uint16_t value);
+void GEN_SaveContext(GenRegister_ctx *context); //copy entire register file in to ctx
+void GEN_LoadContext(const GenRegister_ctx *context); //Restore register context from ctx
+uint16_t *RegPtr(void); // get pointer to raw register array
+
+bool GEN_ParseRegs(uint8_t regindex, __cpu_General_Register *output); // given 4 bit register (0xF), retrun the enum register or false
+
 void GEN_DumpRegs(void);
 
 
-
-
-
-
-
-
+#ifdef __cplusplus
+}
 #endif
+
+#endif // GENERAL_REG_H
