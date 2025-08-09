e07044f039d4d8eaa0678c8c8988b11f8a9b6bc0
jonathan03ant <jonathan03ant@gmail.com>
2025-08-09 15:48:34 -0400

    [general_reg.h] General Register struct name modified

--- src/regs/general_reg.h
+++ src/regs/general_reg.h
@@ -31,7 +31,7 @@ typedef enum
     REG_Gen_R14,
     REG_Gen_R15,
     REG_COUNT_GEN
-}__cpu_General_Register;
+} cpu_General_Register;
 
 
 /*
@@ -42,12 +42,12 @@ typedef struct {
 }GenRegister_ctx; //Register context snapshot
 
 
-void GEN_InitRegs(void); //Reset
-bool Is_Valid(__cpu_General_Register reg); //Bound Check
+void GEN_InitReg(void); //Reset
+bool Is_Valid(cpu_General_Register reg); //Bound Check
 
 //Register Access API (CPU-ALU Facing)
-uint16_t GEN_GetReg(__cpu_General_Register reg);
-void GEN_SetReg(__cpu_General_Register reg, uint16_t value);
+uint16_t GEN_GetReg(cpu_General_Register reg);
+void GEN_SetReg(cpu_General_Register reg, uint16_t value);
 uint16_t *RegPtr(void); // RAW pointer to raw register array
 
 // Register Context Save/Restore
@@ -55,7 +55,7 @@ void GEN_SaveContext(GenRegister_ctx *context); //Save full register state/conte
 void GEN_LoadContext(const GenRegister_ctx *context); //Restore register state/context from ctx
 
 
-bool GEN_ParseRegs(uint8_t regindex, __cpu_General_Register *output); // given 4 bit register address (0xF), retrun the enum register or false
+bool GEN_ParseRegs(uint8_t regindex, cpu_General_Register *output); // given 4 bit register address (0xF), retrun the enum register or false
 
 void GEN_DumpRegs(void);
 
