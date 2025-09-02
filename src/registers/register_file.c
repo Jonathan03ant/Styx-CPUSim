6ac9a3b236f8488261b0857d5ebb45bb36e4d43f
jonathan03ant <jonathan03ant@gmail.com>
2025-09-01 21:23:14 -0400

    [register_file.c] General Function registers implemented

--- src/registers/register_file.c
+++ src/registers/register_file.c
@@ -6,6 +6,7 @@
 
 // Private Register File and helper checkings
 static CPURegisterFile cpu_registers;
+
 static inline bool is_general_register_valid(GeneralRegisterID reg)
 {
     return reg < GENERAL_REG_COUNT;
@@ -15,3 +16,43 @@ static inlone bool is_special_register_valid(SpecialRegisterID reg)
 {
     return reg < SPECIAL_REG_COUNT;
 }
+
+/*
+    * CPURegisterFile initialization (both general and special registers)
+    * Also called by everytime we reset the COUReg file
+*/
+void register_file_init()
+{
+    memset(&cpu_registers, 0, sizeof(cpu_registers));
+}
+
+void register_file_reset()
+{
+    register_file_init();
+}
+
+/*
+    * General Register Operations
+*/
+uint16_t register_file_read_general(GeneralRegisterID regID)
+{
+    if (!is_general_register_valid(reg)){
+        RAISE_ERROR(ERR_INVALID_REGISTER, "register_file_read_general: Invalid Register ID (OutOfBound)");
+        return 0;
+    }
+    return cpu_registers.general.registers[regID];
+}
+
+void register_file_write_general(GeneralRegisterID regID, uint16_t value){
+    if (!is_general_register_valid(regID)){
+        RAISE_ERROR(ERR_INVALID_REGISTER, "register_file_write_general: Invalid Register ID (OutOfBound)");
+        return;
+    }
+    cpu_registers.general.registers[regID] == value;
+}
+
+uint16_t *register_file_get_general_array(void)
+{
+    return cpu_registers.general.registers;
+}
+
