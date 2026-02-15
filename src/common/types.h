2f695f929f10cf0a161da155c96153ff0c387ce6
Jonathan Antenanie <Jonathan.Antenanie@amd.com>
2026-02-14 23:06:06 -0500

    [types.h]: type definition, used across all modules, ISA and instruction field types

--- src/common/types.h
+++ src/common/types.h
@@ -26,3 +26,29 @@ typedef uint8_t byte_t;
 
 // Memory data word (16 bit data word)
 typedef uint16_t word_t;
+
+/*----------------------------------------------------------------------------
+  * ISA TYPES
+  *----------------------------------------------------------------------------*/
+
+// Opcode (4-bit opcode field)
+typedef uint8_t opcode_t;
+
+// General Register address (4 bit, 0-15)
+typedef uint8_t regaddr_t;
+
+// General Register value (16bit register content of GRGSTR pointer)
+typedef uint16_t regval_t;
+
+/*----------------------------------------------------------------------------
+  * INSTRUCTION FIELD TYPE
+  *----------------------------------------------------------------------------*/
+
+// 8-bit Immediate value (I-Type A)
+typedef uint8_t imm8_t;
+
+// 4-bit offset (I-Type B)
+typedef uint8_t offset4_t;
+
+// 12-bit address (J-Type)
+typedef uint16_t addr12_t
