2ad0b0d990bc0c060863d00bf12789455c5cac59
Jonathan Antenanie <Jonathan.Antenanie@amd.com>
2026-02-28 01:16:42 -0500

    [registers.h]: Registers core Public API; GNR REG API's and flag operations

--- src/core/registers/registers.h
+++ src/core/registers/registers.h
@@ -16,4 +16,28 @@ void reg_destroy(RegisterFile_t *rf);
 void reg_reset(RegisterFile_t *rf);
 
 
+// GENERAL PURPOSE REGISTRATION OPS (S0-S15)
+// read and write enforces S0 and validate reg_num
+error_t reg_read(const RegisterFile_t *rf, reg_t reg_num, regval_t *value);
+error_t reg_write(RegisterFile_t *rf, reg_t reg_num, regval_t value);
+
+regval_t reg_read_pc(RegisterFile_t *rf);                                                   // Read PC
+error_t  reg_write_pc(RegisterFile_t *rf, addr_t new_pc);                                   // Write new PC, in case of jump
+void     reg_increment_pc(RegisterFile_t *rf);
+
+// FLAGS REGISTER OPERATIONS
+flags_t reg_get_flags(const RegisterFile_t *rf);                                            // Get Flag Reg vale
+void    reg_set_flags(RegisterFile_t *rf, flags_t flags);                                   // Used by CMP, setting
+flag_bit_t reg_read_flag_bit(const RegisterFile_t *rf, flags_t flag_maask);                 // Read individual bits (ZNCV)
+void    reg_set_flag_bit(const RegisterFile_t *rf, flags_t  flag_mask, flag_bit_t value);
+void    reg_clear_flags(RegisterFile_t *rf);
+
+
+
+
+
+
+
+
+
 #endif /* STYX_REGISTERS_H */
