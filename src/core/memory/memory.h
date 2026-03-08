f653593a5c99449cb883f6c3fbb10b7137f307a7
Jonathan Antenanie <Jonathan.Antenanie@amd.com>
2026-03-08 00:27:13 -0500

    [memory.h]: Mem Subsystem Public API; function defs for acccess, and protection

--- src/core/memory/memory.h
+++ src/core/memory/memory.h
@@ -30,9 +30,24 @@ typedef enum {
 } MemoryMode_e;
 
 
+Memory_t mem_create(void);
+error_t mem_init(Memory_t *mem);
+void mem_destroy(Memory_t *mem);
+error_t mem_reset(Memory_t *mem);
 
+/* ===== Byte Access Operations ===== */
+error_t mem_read_byte(Memory_t *mem, addr_t addr, byte_t *value);
+error_t mem_write_byte(Memory_t *mem, addr_t addr, byte_t value);
 
+/* ===== Word Access Operations ===== */
+// Both enforces Little-Endian (low byte, addr +1 or high byte)
+error_t mem_read_word(Memory_t *mem, addr_t addr, word_t *value);
+error_t mem_write_word(Memory_t *mem, addr_t addr, word_t value);
 
+/* Memory Protection */
+error_t mem_set_protection(Memory_t *mem, MemoryMode_e mode);
+error_t mem_get_protection(Memory_t *mem, MemoryMode_e mode);
 
-
+/* Program Loader */
+error_t mem_load_program(Memory_t *mem, const char *filename);
 #endif /* STYX_MEMORY_H */
