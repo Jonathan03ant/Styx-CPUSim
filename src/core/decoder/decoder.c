617b8a37ada0b922e4868f5080850c1df3925061
Jonathan Antenanie <jonathan03ant@gmail.com>
2026-05-31 04:20:22 +0000

    [decoder]: Adding decoder.c inst classification functions

--- src/core/decoder/decoder.c
+++ src/core/decoder/decoder.c
@@ -19,7 +19,7 @@ static error_t decode_itype_b(insn_t raw, DecodedInstruction_t *decoded);
 static error_t decode_jtype(insn_t raw, DecodedInstruction_t *decoded);
 
 // Opcode to format lookup table
-static const InsnFormat_e opcode_format_table[16] = {
+static const InsnFormat_e opcode_LUT[16] = {
     [OP_NOP]   = INSN_FORMAT_I_A,           // Opcode 0 (NOP) is I-Type A
     [OP_MOV]   = INSN_FORMAT_R,             // Opcode 1 (MOV) is R-Type
     [OP_LOAD]  = INSN_FORMAT_I_B,           // Opcode 2 (LOAD) is I-Type B
@@ -38,17 +38,77 @@ static const InsnFormat_e opcode_format_table[16] = {
     [OP_LUI]   = INSN_FORMAT_I_A,           // Opcode 15 (LUI) is I-Type A
 };
 
+/*
+  Validate a decoded instruction opcode and format
+  * TO be adjusted later as needed
+*/
 bool is_valid_instruction(const DecodedInstruction_t *decoded)
 {
-  //What makes an instruction valid
+    if (!is_valid_opcode(decoded->opcode)){
+        return false;
+    }
+
+    InsnFormat_e expected_format = get_inst_format(decoded->opcode);
+    if (decoded->format != expected_format) {
+        return false;
+    }
+
+    return true;
 }
 
+/*
+  Validate giben opcode is valide
+  Check with OP_COUNT
+*/
 bool is_valid_opcode(opcode_t opcode)
 {
     return opcode < OP_COUNT;
 }
 
+/*
+  Validate given register is valid
+  Calls register validation function
+*/
 bool is_valid_register(reg_t reg_num)
 {
     return reg_is_valid_num(reg_num);
+}
+
+/*
+  InsnFormat_e LUT
+*/
+InsnFormat_e get_inst_format(opcode_t opcode)
+{
+    if (is_valid_opcode(opcode)){
+        return opcode_LUT[opcode];
+    } else {
+        return INSN_FORMAT_INVALID;
+    }
+}
+
+/*
+  Check if instruction has immediate field (I-Types only)
+*/
+bool instruction_has_imm(opcode_t opcode)
+{
+    InsnFormat_e format = get_inst_format(opcode);
+    return (format == INSN_FORMAT_I_A || format == INSN_FORMAT_I_B);
+}
+
+/*
+  Check if instruction is LOAD/STORE
+*/
+bool instruction_is_mem_access(opcode_t opcode)
+{
+    InsnFormat_e format = get_inst_format(opcode);
+    return (format == INSN_FORMAT_I_B);
+}
+
+/*
+  Check if instruction is control flow
+*/
+bool instruction_is_ctl_flow(opcode_t opcode)
+{
+    InsnFormat_e format = get_inst_format(opcode);
+    return (format == INSN_FORMAT_J);
 }
