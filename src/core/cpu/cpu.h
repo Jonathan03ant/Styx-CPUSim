67a5ced4bae3be1ad8603c3cbff0d52193fb4b34
Jonathan Antenanie <jonathan03ant@gmail.com>
2026-07-03 15:59:43 +0000

    [CPU] First initial Module

---
+++ src/core/cpu/cpu.h
@@ -0,0 +1,17 @@
+/**
+  * @file cpu.h
+  * @brief CPU - TOP-Level orchestrator
+  *
+  * The CPU module ties together all components (Registers, memory, decoder, ALU)
+  * and provides the main execution interface (step, run, reset)
+  */
+
+#ifndef CPU_H
+#define CPU_H
+
+
+
+
+
+
+#endif // CPU_H
